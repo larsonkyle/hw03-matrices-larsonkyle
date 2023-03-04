78be69834a5b95354cd4ee120be34f197252fcf4
larsonkyle <klarson22@saddleback.edu>
2023-03-04 03:16:22 -0800

    created all functions except determinant

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -11,17 +11,57 @@ void fileIO(std::string filename, int A[][AR_CAP], int& n1, int& m1, int B[][AR_
 
 		for (int rows = 0; rows < m1; rows++) 
 			for (int cols = 0; cols < n1; cols++)
-				iFile >> A[cols][rows];
+				iFile >> A[rows][cols];
 
 		iFile >> n2;
 		iFile >> m2;
 
 		for (int rows = 0; rows < m2; rows++)
 			for (int cols = 0; cols < n2; cols)
-				iFile >> B[cols][rows];
+				iFile >> B[rows][cols];
 	}
 	else
 		std::cout << "ERROR: file did not open." << std::endl;
 
 iFile.close();
+}
+
+void matrixAddition(const int A[][AR_CAP], int n1, int m1, const int B[][AR_CAP], int n2, int m2, int res[][AR_CAP]){
+	for (int rows = 0; rows < m1; rows++)
+		for (int cols = 0; cols < n1; cols++) 
+			res[rows][cols] = A[rows][cols] + B[rows][cols];
+}
+
+void matrixSubtraction(int A[][AR_CAP], int n1, int m1, int B[][AR_CAP], int n2, int m2, int res[][AR_CAP]){
+	for (int rows = 0; rows < m1; rows++) 
+		for (int cols = 0; cols < n1; cols++) 
+			res[rows][cols] = A[rows][cols] - B[rows][cols];
+}
+
+void matrixMultiplication(int A[][AR_CAP], int n1, int m1, int B[][AR_CAP], int n2, int m2, int res[][AR_CAP]){
+	for (int rows = 0; rows < m1; rows++)      
+		for (int cols = 0; cols < n1; cols++) 
+			res[rows][cols] = A[rows][cols] * B[rows][cols];
+}
+
+void matrixTranspose(int M[][AR_CAP], int n, int m, int res[][AR_CAP]){
+	for (int rows = 0; rows < m; rows++)      
+		for (int cols = 0; cols < n; cols++)
+			res[rows][cols] = M[cols][rows];
+}
+
+double matrixDeterminant3D(int M[][AR_CAP], int n, int m){
+	return 0.00;
+}
+
+void print2DMatrix(const int M[][AR_CAP], int n1, int m1) {
+	std::cout << "2D Matrix: " << std::endl;
+
+	for (int rows = 0; rows < m1; rows++){
+		for (int cols = 0; cols < n1; cols++){
+			std::cout << M[rows][cols];
+		}
+		std::cout << std::endl;
+	}
+
 }
