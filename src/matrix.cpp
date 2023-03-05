883cb12d1f3db0d72789f868b82a2ead9f235467
larsonkyle <klarson22@saddleback.edu>
2023-03-04 18:13:37 -0800

    fixed fileIO bug

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -2,27 +2,29 @@
 
 void fileIO(string filename, int A[][AR_CAP], int& n1, int& m1, int B[][AR_CAP], int& n2, int& m2) {
 	ifstream iFile;
-//	filename = "input.txt";
 
-	iFile.open(filename);
-	//if (iFile.is_open()) {
+	iFile.open(filename.c_str());
+	if (iFile.is_open()) {
 		iFile >> n1;
 		iFile >> m1;
 
-		for (int rows = 0; rows < m1; rows++) 
-			for (int cols = 0; cols < n1; cols++)
+		for (int rows = 0; rows < m1; rows++) { 
+			for (int cols = 0; cols < n1; cols++) {
 				iFile >> A[rows][cols];
-
+			}
+		}
 		iFile >> n2;
 		iFile >> m2;
 
-		for (int rows = 0; rows < m2; rows++)
-			for (int cols = 0; cols < n2; cols)
+		for (int rows = 0; rows < m2; rows++) {
+			for (int cols = 0; cols < n2; cols++) {
 				iFile >> B[rows][cols];
-	//}
-	//else
-		//std::cout << "ERROR: file did not open." << std::endl;
-
+			}
+		}
+	}
+	else {
+		std::cout << "ERROR: file did not open." << std::endl;
+	}
 iFile.close();
 }
 
@@ -65,3 +67,4 @@ void print2DMatrix(const int M[][AR_CAP], int n1, int m1) {
 	}
 
 }
+
